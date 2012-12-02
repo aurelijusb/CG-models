e56c87c96c7649efa63d646f02315e41e1051dcc
Aurelijus Banelis <aurelijus@banelis.lt>
2012-12-02 15:02:01 +0200

    Object from numeric vertex array

--- README.rst
+++ README.rst
@@ -11,9 +11,10 @@ Libraries
 To compile program::
 
     gcc src/main.c -lGL -lGLU -lglut -lm -o U3Aurelijus
-  
+    g++ grafika2.cpp -lGL -lGLU -lglut -lm -o U3T
   
 References
 ----------
 
  * http://uosis.mif.vu.lt/~rimask/destymas/kompiuterine-grafika-ps/3-uzd/
+ * https://svn.blender.org/svnroot/bf-extensions/trunk/py/scripts/addons/add_mesh_solid.py
--- src/main.c
+++ src/main.c
@@ -5,6 +5,14 @@
  * @author Aurelijus Banelis
  */
 
+//Tada parašome OpenGL  programą, kuri:
+//    rhombic triacontahedron
+//    vaizduoja briaunainio 3d modelį, kurio gretimos sienos turi skirtingas spalvas
+//    sukurti vartotojo sąsają, kuri leidžia briaunainį apžiūrėti iš visų pusių (pvz. view_torus.cpp)
+//    nustatyti tris posūkius (ašis+kampas), kurie generuoja visus posūkius pervedančius briaunainį į save
+//    žaidimas: pasukti briaunainį į specialią padėtį animuojant pasirenkamus posūkius (vartotojo sąsaja turi būti patogi)
+
+
 #include <GL/gl.h>
 #include <GL/glut.h>
 #include <math.h>
@@ -121,40 +129,111 @@ void drawAxis() {
 }
 
 void drawObject() {
-    // X (by y)
-    glColor3f(0.8, 0.2, 0.2);
-    glBegin(GL_LINE_LOOP);
-        glVertex3f(0, 0, 0);
-        glVertex3f(0, 0.5, 0);
-        glVertex3f(0.1, 0.5, 0);
-        glVertex3f(0, 1, 0);
-        glVertex3f(-0.1, 0.5, 0);
-        glVertex3f(0, 0.5, 0);
+    
+//        # Calculate the necessary constants
+//        s = 1/sqrt(3)
+//        t = sqrt((3-sqrt(5))/6)
+//        u = sqrt((3+sqrt(5))/6)
+//
+//        # create the vertices and faces
+//        v = [(s,s,s),(s,s,-s),(s,-s,s),(s,-s,-s),(-s,s,s),(-s,s,-s),(-s,-s,s),(-s,-s,-s),
+//             (t,u,0),(-t,u,0),(t,-u,0),(-t,-u,0),(u,0,t),(u,0,-t),(-u,0,t),(-u,0,-t),(0,t,u),
+//             (0,-t,u),(0,t,-u),(0,-t,-u)]
+//        faces = [[0,8,9,4,16],[0,12,13,1,8],[0,16,17,2,12],[8,1,18,5,9],[12,2,10,3,13],
+//                 [16,4,14,6,17],[9,5,15,14,4],[6,11,10,2,17],[3,19,18,1,13],[7,15,5,18,19],
+//                 [7,11,6,14,15],[7,19,3,10,11]]
+
+//>>> for v in o.data.vertices:
+//...     print ("{%f, %f, %f}, " % (v.co[0], v.co[1], v.co[2]))
+    
+#define NV 32
+    float vertexes[NV][3] = {{0.525731, 0.525731, 0.525731}, 
+                            {0.525731, 0.525731, -0.525731}, 
+                            {0.525731, -0.525731, 0.525731}, 
+                            {0.525731, -0.525731, -0.525731}, 
+                            {-0.525731, 0.525731, 0.525731}, 
+                            {-0.525731, 0.525731, -0.525731}, 
+                            {-0.525731, -0.525731, 0.525731}, 
+                            {-0.525731, -0.525731, -0.525731}, 
+                            {0.324920, 0.850651, -0.000000}, 
+                            {-0.324920, 0.850651, -0.000000}, 
+                            {0.324920, -0.850651, -0.000000}, 
+                            {-0.324920, -0.850651, 0.000000}, 
+                            {0.850651, -0.000000, 0.324920}, 
+                            {0.850651, -0.000000, -0.324920}, 
+                            {-0.850651, -0.000000, 0.324920}, 
+                            {-0.850651, 0.000000, -0.324920}, 
+                            {-0.000000, 0.324920, 0.850651}, 
+                            {-0.000000, -0.324920, 0.850651}, 
+                            {-0.000000, 0.324920, -0.850651}, 
+                            {0.000000, -0.324920, -0.850651}, 
+                            {-0.000000, 0.850651, 0.525731}, 
+                            {0.850651, 0.525731, -0.000000}, 
+                            {0.525731, -0.000000, 0.850651}, 
+                            {-0.000000, 0.850651, -0.525731}, 
+                            {0.850651, -0.525731, -0.000000}, 
+                            {-0.525731, -0.000000, 0.850651}, 
+                            {-0.850651, 0.525731, -0.000000}, 
+                            {-0.000000, -0.850651, 0.525731}, 
+                            {0.525731, -0.000000, -0.850651}, 
+                            {-0.525731, -0.000000, -0.850651}, 
+                            {-0.850651, -0.525731, -0.000000}, 
+                            {-0.000000, -0.850651, -0.525731}};    
+//>>> for f in o.data.faces:
+//...     print ("{%d, %d, %d, %d}, " % (f.vertices[0], f.vertices[1], f.vertices[2], f.vertices[3]))
+#define NF 30
+    short faces[NF][4] = {{0, 20, 16, 22}, 
+                          {0, 22, 12, 21}, 
+                          {0, 21, 8, 20}, 
+                          {1, 21, 13, 28}, 
+                          {1, 28, 18, 23}, 
+                          {1, 23, 8, 21}, 
+                          {2, 22, 17, 27}, 
+                          {2, 27, 10, 24}, 
+                          {2, 24, 12, 22}, 
+                          {3, 24, 10, 31}, 
+                          {3, 31, 19, 28}, 
+                          {3, 28, 13, 24}, 
+                          {4, 20, 9, 26}, 
+                          {4, 26, 14, 25}, 
+                          {4, 25, 16, 20}, 
+                          {5, 23, 18, 29}, 
+                          {5, 29, 15, 26}, 
+                          {5, 26, 9, 23}, 
+                          {6, 25, 14, 30}, 
+                          {6, 30, 11, 27}, 
+                          {6, 27, 17, 25}, 
+                          {7, 29, 19, 31}, 
+                          {7, 31, 11, 30}, 
+                          {7, 30, 15, 29}, 
+                          {8, 23, 9, 20}, 
+                          {10, 27, 11, 31}, 
+                          {12, 24, 13, 21}, 
+                          {14, 26, 15, 30}, 
+                          {16, 25, 17, 22}, 
+                          {18, 28, 19, 29}};
+
+    
+#define GL_VERTER_ARRAY(array) glVertex3f(array[0], array[1], array[2]) 
+    
+    //Vertexes
+    glColor3f(1, 1, 0);
+    glBegin(GL_POINTS);
+        int i;
+        for (i = 0; i < NV; i++) {
+            GL_VERTER_ARRAY(vertexes[i]);
+        }
     glEnd();
     
-    
-    //Y (by x)
-    glColor3f(0.2, 0.8, 0.2);
-    glBegin(GL_LINE_LOOP);
-        glVertex3f(0, 0, 0);
-        glVertex3f(0.5, 0, 0);
-        glVertex3f(0.5, 0.1, 0);
-        glVertex3f(1, 0, 0);
-        glVertex3f(0.5, -0.1, 0);
-        glVertex3f(0.5, 0, 0);
-    glEnd();
-    
-    
-    //Z (by XY)
-    glColor3f(0.4, 0.4, 0.8);
-    glBegin(GL_LINE_LOOP);
-        glVertex3f(0, 0, 0);
-        glVertex3f(0, 0, 0.5);
-        glVertex3f(0.1, 0.1, 0.5);
-        glVertex3f(0, 0, 1);
-        glVertex3f(-0.1, -0.1, 0.5);
-        glVertex3f(0, 0, 0.5);
-    glEnd();
+    glColor3f(0, 1, 1);
+    for (i=0; i < NF; i++) {
+        glBegin(GL_LINES);
+            int j;
+            for (j=0; j < 4; j++) {
+                GL_VERTER_ARRAY(vertexes[faces[i][j]]);
+            }
+        glEnd();
+    }
 }
 
 
