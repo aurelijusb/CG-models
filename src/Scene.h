6ae2fbcfe78106d8c136ddb6cae59af82ac21ecd
Aurelijus Banelis <aurelijus@banelis.lt>
2013-01-24 00:22:43 +0200

    Dirty c to cpp migration. Obj file loading.

--- src/Scene.h
+++ src/Scene.h
@@ -5,13 +5,33 @@
 #ifndef SCENE_H
 #define	SCENE_H
 
+#include <iostream>
+#include <vector>
+#include "Objects3D.h"
+#include <GL/gl.h>
+
+using namespace std;
+using namespace Objects3D;
+
 class Scene {
 public:
     Scene();
-    Scene(const Scene& orig);
+    
+    void init();
+    void draw();
+    void onKeyPress(unsigned char key, int keyX, int keyY);
+    void onMousePress(int button, int state, int x, int y);
+    void onMouseMove(int x, int y);
+    void reshape(int w, int h);
+    void timer(int value);
+    
     virtual ~Scene();
 private:
-
+    vector<Object3D*> models;
+    vector<Material*> materials;
+    GLfloat *lightAmbient;
+    GLfloat *lightDiffuse;
+    GLfloat *lightPosition;
 };
 
 #endif	/* SCENE_H */
--- src/main.cpp
+++ src/main.cpp
@@ -2,16 +2,51 @@
  * @author Aurelijus Banelis
  */
 
-#include <cstdlib>
+#include <GL/gl.h>
+#include <GL/glut.h>
+
 #include "main.h"
+#include "Scene.h"
 
-using namespace std;
+Scene *scene;
 
-/*
- * 
- */
+void draw() {
+    scene->draw();
+}
+
+void onKeyPress(unsigned char key, int keyX, int keyY) {
+    scene->onKeyPress(key, keyX, keyY);
+}
+
+void onMousePress(int button, int state, int x, int y) {
+    scene->onMousePress(button, state, x, y);
+}
+
+void onMouseMove(int x, int y) {
+    scene->onMouseMove(x, y);
+}
+
+void reshape(int w, int h) {
+    scene->reshape(w, h);
+}
+
+void timer(int value) {
+    scene->timer(value);
+    glutTimerFunc (40, timer, value);
+}
+
 int main(int argc, char** argv) {
-    main2(argc, argv);
+    glutInit(&argc, argv);
+    scene = new Scene();
+    scene->init();
+    glutDisplayFunc(draw);
+    glutReshapeFunc(reshape);
+    glutMouseFunc(onMousePress);
+    glutMotionFunc(onMouseMove);
+    glutTimerFunc (40, timer, 0);
+    glutKeyboardFunc(onKeyPress);
+    glutMainLoop();
+    delete scene;
     return 0;
 }
 
